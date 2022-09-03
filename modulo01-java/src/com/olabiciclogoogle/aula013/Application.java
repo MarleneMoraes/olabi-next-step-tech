81147dab350c25e14b34a2e31c2b44110d3a6937
Marlene Moraes <marlenevmoraes@outlook.com>
2022-09-03 12:25:14 -0300

    class 013

---
+++ modulo01-java/src/com/olabiciclogoogle/aula013/Application.java
@@ -0,0 +1,71 @@
+package com.olabiciclogoogle.aula013;
+
+/**
+ * @author Marlene
+ * @date 05-13-2022
+ * 
+ * Modulo 2: Programacao Orientada a Objetos 
+ * Aula 013: Objetos
+ */
+
+public class Application {
+	public static void main(String[] args) {
+		Cachorro.fazerBarulho();
+		
+		//Instanciar uma classe 
+		Pessoa pessoa = new Pessoa();
+		pessoa.name = "Julio Cesar Brito da Silva";
+		
+		Pessoa aluno1 = new Pessoa();
+		aluno1.name = "Larissa Botelho";
+		
+		Pessoa aluno2 = new Pessoa();
+		aluno2.name = "Diego Simoes";
+		
+		
+		//Inicializacao por referencia
+		HomemAranha homemAranha1 = new HomemAranha();
+		homemAranha1.name = "Tobey Maguire";
+		
+		HomemAranha homemAranha2 = new HomemAranha();
+		homemAranha2.universe = 2;
+		
+		//Inicializacao pelo metodo construtor
+		HomemAranha homemAranha3 = new HomemAranha(3, "Tom Holland");
+	}
+}
+
+
+// Exemplo de Objetos em Java
+
+class Cachorro {
+	
+	//Metodo: Comportamentos de um objeto
+	public static void fazerBarulho() {
+		System.out.println("Aul aul");
+	}
+		
+}
+
+class Pessoa {
+	//Atributos
+	String name;
+
+	public Pessoa() { }
+	
+}
+
+
+class Car {
+	private String nameCar;
+
+	//Metodos gets e sets - protecao dos atributos desta classe
+	public String getNomeCarro() {
+		return nameCar;
+	}
+
+	public void setNomeCarro(String nomeCarro) {
+		this.nameCar = nomeCarro;
+	}
+	
+}
