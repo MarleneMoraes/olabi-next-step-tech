b69a6856e88c5a64f5e90858581ad36b6d64bfc9
Marlene Moraes <marlenevmoraes@outlook.com>
2022-09-03 21:20:38 -0300

    class 24

---
+++ modulo03-cpp/aula024/Ex001.cpp
@@ -0,0 +1,24 @@
+// 1 - Escreva um programa em C++ para reverter uma determinada string.
+
+#include <iostream>
+#include <string>
+#include<array>
+
+using namespace std;
+
+string reverse(string s){
+    string reverseString;
+
+    for(int i = s.size(); i == 0; i--){
+        cout << s[i];
+    }
+
+    return reverseString;
+}
+
+int main() {
+    string string = "Marlene";
+
+    cout << reverse(string);
+}
+
